8c2cee224f36213f228ad50b9a8dfb3db9677af4
Karsten Gebbert <karsten@null2.net>
2013-08-11 16:17:17 +0200

    working zmq
    Signed-off-by: Karsten Gebbert <karsten@null2.net>

--- client.js
+++ client.js
@@ -1,7 +1,7 @@
 var zmq = require('zmq'),
     sock = zmq.socket('pull');
 
-sock.connect('tcp://127.0.0.1:3000');
+sock.connect('tcp://127.0.0.1:8888');
 
 sock.on('message', function(msg){
   console.log('trig %s', msg.toString());
--- main.scm
+++ main.scm
@@ -5,16 +5,15 @@
  
 (define (main)
   (let ((device (open-device "/dev/midi1"))
-        (socket (make-socket 'push (make-context 4))))
-    (bind-socket socket "tcp://127.0.0.1:3000")
-    (connect-socket socket "tcp://127.0.0.1:3000")
+        (socket (make-socket 'push (make-context 4)))) ; make a push type socket
+    (bind-socket socket "tcp://127.0.0.1:8888") ; the sender/pusher binds
     (thread-start! 
      (let loop ()
        (when #t
-         (send-message socket "tick" #t)
+         (send-message socket "tick")
          (write-midi device (make-note 1 30 100))
          (thread-sleep! 1)
-         (send-message socket "tock" #t)
+         (send-message socket "tock")
          (write-midi device (make-note 1 30 0))
          (thread-sleep! 1)
          (loop))))))
