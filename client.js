06cc514be3c649da8d60d9d8a438510134e6f8a6
Karsten Gebbert <karsten@null2.net>
2013-08-23 23:31:12 +0200

    remove js please

--- client.js
+++
@@ -1,8 +0,0 @@
-var zmq = require('zmq'),
-    sock = zmq.socket('pull');
-
-sock.connect('tcp://127.0.0.1:8888');
-
-sock.on('message', function(msg){
-  console.log('trig %s', msg.toString());
-});
